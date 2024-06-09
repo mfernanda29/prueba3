ce7d4a11d25300c350d392ab551153c094307081
mfernanda29 <117603869+mfernanda29@users.noreply.github.com>
2024-06-09 16:33:30 -0500

    cuarto commit

--- modalidad.py
+++ modalidad.py
@@ -6,16 +6,16 @@ from sklearn.base import TransformerMixin
 from sklearn.preprocessing import RobustScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-# Configuración de pandas para mostrar todas las columnas
+# Configuración de pandas para mostrar todas las columnas y ajustar el ancho
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
 
 # Cargar el archivo CSV desde la carpeta data
 df = pd.read_csv('data/Accidentes de tránsito en carreteras-2020-2021-Sutran.csv', encoding='utf-8-sig', delimiter=';')
 
 # Mostrar las primeras 5 filas del DataFrame
 print("Primera vista del DataFrame original:")
-print(df.head(5))
+print(df.head(50).to_string(index=False))
 
 # Definir las columnas a eliminar basándonos en los nombres exactos impresos
 DROP_COLUMNS = ['FECHA_CORTE', 'FECHA', 'KILOMETRO', 'FALLECIDOS', 'HERIDOS']
@@ -24,8 +24,8 @@ DROP_COLUMNS = ['FECHA_CORTE', 'FECHA', 'KILOMETRO', 'FALLECIDOS', 'HERIDOS']
 df.drop(columns=DROP_COLUMNS, inplace=True)
 
 # Mostrar las primeras 5 filas del DataFrame después de eliminar las columnas
-print("Vista del DataFrame después de eliminar columnas:")
-print(df.head(5))
+print("\nVista del DataFrame después de eliminar columnas:")
+print(df.head(50).to_string(index=False))
 
 # Aplicar One-Hot Encoding al campo 'MODALIDAD'
 df_one_hot_modalidad = pd.get_dummies(df, columns=['MODALIDAD'])
@@ -36,5 +36,5 @@ for column in df_one_hot_modalidad.columns:
         df_one_hot_modalidad[column] = df_one_hot_modalidad[column].astype(int)
 
 # Mostrar las primeras 5 filas para verificar el resultado
-print("Vista del DataFrame después de One-Hot Encoding:")
-print(df_one_hot_modalidad.head(5))
+print("\nVista del DataFrame después de One-Hot Encoding:")
+print(df_one_hot_modalidad.head(50).to_string(index=False))
