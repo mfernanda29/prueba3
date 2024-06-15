3cf2a43d8781625af3c386c484aa47d9ce5bfed7
mfernanda29 <117603869+mfernanda29@users.noreply.github.com>
2024-06-15 13:05:00 -0500

    Update modalidad.py

--- modalidad.py
+++ modalidad.py
@@ -1,11 +1,29 @@
+import csv
 import pandas as pd
+import numpy as np
+import io
+from sklearn.pipeline import Pipeline
+from sklearn import preprocessing
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import RobustScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from datetime import datetime
 
-# Cargar el DataFrame
-df_ACC_TRA = pd.read_csv('/mnt/data/Data/Accidentes_de_transito_en_carreteras-2020-2021-Sutran.csv', encoding='utf8', delimiter=';')
+df_ACC_TRA = pd.read_csv('Data/Accidentes_de_transito_en_carreteras-2020-2021-Sutran.csv', encoding='utf8', delimiter=';')
+
+columnaCodigoVia = []
+
+df_ACC_TRA.head(100)
+
+print(df_ACC_TRA.head(100))
 
 # Configuración de pandas para mostrar todas las columnas y ajustar el ancho
-pd.set_option('display.max_columns', 20)  # Mostrar un máximo de 20 columnas para una mejor visualización
+pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
+
+# Mostrar las primeras 5 filas del DataFrame
+print("Primera vista del DataFrame original:")
+print(df_ACC_TRA.head(100).to_string(index=False))
 
 # Definir las columnas a eliminar basándonos en los nombres exactos impresos
 DROP_COLUMNS = ['FECHA_CORTE', 'FECHA', 'KILOMETRO', 'FALLECIDOS', 'HERIDOS']
@@ -13,15 +31,44 @@ DROP_COLUMNS = ['FECHA_CORTE', 'FECHA', 'KILOMETRO', 'FALLECIDOS', 'HERIDOS']
 # Eliminar las columnas especificadas
 df_ACC_TRA.drop(columns=DROP_COLUMNS, inplace=True)
 
+# Mostrar las primeras 5 filas del DataFrame después de eliminar las columnas
+print("\nVista del DataFrame después de eliminar columnas:")
+print(df_ACC_TRA.head(100).to_string(index=False))
+
 # Listado donde estan almacenados los campos relacionado al dataset de accidentes de transito
 columnas_ACC_TRA = list(df_ACC_TRA.select_dtypes(include=['object']).columns)
 
+def convertir_horas_a_minutos(tiempo):
+    if tiempo == 'N.I.':
+      return -1
+
+    try:
+        horas, minutos = map(int, tiempo.split(':'))
+        total_minutos = horas * 60 + minutos
+        return total_minutos
+    except ValueError:
+        return -1
+
 def procesar_datos():
-    global df_ACC_TRA
+    global df_ACC_TRA, columnaCodigoVia
+
+    #Crear nueva columna de hora en minutos 
+    df_ACC_TRA["HORA_MINUTOS"] = df_ACC_TRA["HORA"].apply(convertir_horas_a_minutos)
+
+    # Almacenar en una lista los registros del codigo de via sin repetir los datos
+    columnaCodigoVia = list(df_ACC_TRA['CODIGO_VIA'].value_counts().index)
 
     # Eliminar registros que sean duplicados
     df_ACC_TRA = df_ACC_TRA.drop_duplicates() if df_ACC_TRA.duplicated().any() else df_ACC_TRA
 
+    # Almacenar en un diccionario los codigos via, en el cual serán enumerados del 1 al 175
+    diccionario_codigo_via = {element: index+1 for index, element in enumerate(columnaCodigoVia)}
+    # Convertir la columna de codigo_via que estan en cadena en un label encoded data
+    df_ACC_TRA["CODIGO_VIA"] = df_ACC_TRA["CODIGO_VIA"].map(diccionario_codigo_via)
+
+    # Existencias de departamentos en minusculas, por lo que forzamos las mayusculas
+    df_ACC_TRA['DEPARTAMENTO'] = df_ACC_TRA['DEPARTAMENTO'].str.upper()
+    
     # Aplicar One-Hot Encoding a los campos 'MODALIDAD' y 'DEPARTAMENTO'
     df_ACC_TRA = pd.get_dummies(df_ACC_TRA, columns=['MODALIDAD', 'DEPARTAMENTO'])
 
@@ -31,8 +78,7 @@ def procesar_datos():
             df_ACC_TRA[column] = df_ACC_TRA[column].astype(int)
 
 procesar_datos()
-
-# Mostrar las primeras 100 filas seleccionando solo algunas columnas para verificar el resultado
+# Mostrar las primeras 100 filas para verificar el resultado
 print("\nVista del DataFrame después de One-Hot Encoding:")
-print(df_ACC_TRA.head(100)[['HORA', 'CODIGO_VIA'] + [col for col in df_ACC_TRA.columns if 'MODALIDAD_' in col or 'DEPARTAMENTO_' in col][:10]].to_string(index=False))
+print(df_ACC_TRA.head(100).to_string(index=False))
 
